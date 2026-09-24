ca41ec2313ee56c94eda702dea87127921aa400b
Henok Abrham <abrhaamhenok@gmail.com>
2026-09-24 11:25:49 +0300

    feat: add normalization to CIFAR-10 dataset transformation

--- model/baseline.py
+++ model/baseline.py
@@ -26,7 +26,13 @@ model.eval()
 # 2. CIFAR-10 dataset
 # -------------------------
 
-transform = transforms.ToTensor()
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(
+        (0.4914, 0.4822, 0.4465),
+        (0.2023, 0.1994, 0.2010)
+    )
+])
 
 test_dataset = datasets.CIFAR10(
     root="data",
